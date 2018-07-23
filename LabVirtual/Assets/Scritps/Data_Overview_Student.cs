30cedcf32a3d55a667fa498c576ad2522766d625
Juan Joa <juandanieljoa@gmail.com>
2018-07-23 19:57:45 -0400

    Overview student almost done

---
+++ LabVirtual/Assets/Scritps/Data_Overview_Student.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class Data_Overview_Student : MonoBehaviour {
+
+    Dictionary<string,  int> Student_grades;
+    int changeCounter = 0;
+
+    public Button buttonMenu;
+
+    public GameObject menu;
+
+
+
+    // Use this for initialization
+    void Start () {
+        SetGrades("Nomenclatura", 3);
+        SetGrades("Balance de Ecuaciones", 2);
+        SetGrades("Tabla Periodica", 1);
+        SetGrades("Conversion de Unidades", 4);
+        SetGrades("Estequiometria", 5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Init()
+    {
+        if (Student_grades != null)
+        {
+            return;
+        }
+        Student_grades = new Dictionary<string,  int>();
+    }
+
+    public int GetGrades(string subject)
+    {
+        Init();
+        if (Student_grades.ContainsKey(subject) == false)
+        {
+            return 0;
+        }
+        
+
+        return Student_grades[subject];
+    }
+
+    public void SetGrades(string subject, int grade)
+    {
+        Init();
+
+        changeCounter++;
+
+        if (Student_grades.ContainsKey(subject) == false)
+        {
+            Student_grades[subject] = new int();
+        }
+        Student_grades[subject] = grade;
+    }
+
+    public void ChangeGrades(string subject, int new_grade)
+    {
+        Init();
+        int currGrade = GetGrades(subject);
+        SetGrades(subject, currGrade + new_grade);
+    }
+
+    public string[] GetStudentGrades()
+    {
+        Init();
+        return Student_grades.Keys.ToArray();
+    }
+
+    public string[] GetGroups_codes(string SortByGrade)
+    {
+        Init();
+        return Student_grades.Keys.OrderByDescending(n => GetGrades(SortByGrade)).ToArray();
+
+    }
+
+    public int GetChangeCounter()
+    {
+        return changeCounter;
+    }
+
+}
---
+++ LabVirtual/Assets/Scritps/SubjectList.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubjectList : MonoBehaviour {
+
+    public GameObject SubjectGradePrefab;
+
+    Data_Overview_Student datagrid;
+
+    int lastChangeCounter;
+
+    // Use this for initialization
+    void Start () {
+
+        datagrid = GameObject.FindObjectOfType<Data_Overview_Student>();
+
+        lastChangeCounter = datagrid.GetChangeCounter() - 1;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (datagrid.GetChangeCounter() == lastChangeCounter)
+        {
+            return;
+        }
+        else
+        {
+            lastChangeCounter = datagrid.GetChangeCounter();
+
+            while (this.transform.childCount > 0)
+            {
+                Transform firstchild = this.transform.GetChild(0);
+                firstchild.SetParent(null);
+                Destroy(firstchild.gameObject);
+            }
+
+            ShowGrade("Nomenclatura");
+            ShowGrade("Balance de Ecuaciones");
+            ShowGrade("Tabla Periodica");
+            ShowGrade("Conversion de Unidades");
+            ShowGrade("Estequiometria");
+
+        }
+    }
+
+    void ShowGrade(string subject)
+    {
+        int grade = datagrid.GetGrades(subject);
+
+        GameObject gameObject = (GameObject)Instantiate(SubjectGradePrefab, transform);
+        gameObject.transform.SetParent(this.transform);
+
+        switch(subject)
+        {
+            case "Nomenclatura":
+                gameObject.transform.Find("TextNomenclaturaGrade").GetComponent<Text>().text = grade.ToString();
+                break;
+            case "Balance de Ecuaciones":
+                gameObject.transform.Find("TextBalanceEGrade").GetComponent<Text>().text = grade.ToString();
+                break;
+            case "Tabla Periodica":
+                gameObject.transform.Find("TextTablaPeriodicaGrade").GetComponent<Text>().text = grade.ToString();
+                break;
+            case "Conversion de Unidades":
+                gameObject.transform.Find("TextConversionUGrade").GetComponent<Text>().text = grade.ToString();
+                break;
+            case "Estequiometria":
+                gameObject.transform.Find("TextEstequimetriaGrade").GetComponent<Text>().text = grade.ToString();
+                break;
+        }        
+    }
+}
