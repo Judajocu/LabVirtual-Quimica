e30323021ccf66244a6830cdce02655d257b6938
Juan Joa <juandanieljoa@gmail.com>
2018-07-23 08:30:50 -0400

    datagrid working

--- LabVirtual/Assets/Scritps/Datagrid_Overview_Professor.cs
+++ LabVirtual/Assets/Scritps/Datagrid_Overview_Professor.cs
@@ -1,25 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Datagrid_Overview_Professor : MonoBehaviour {
 
-    public GameObject prefab_groups;
+    /*public GameObject prefab_groups;
     public GameObject prefab_groupcode;
     public GameObject prefab_avggrades;
-    public int Cant_grupos;
+    public int Cant_grupos;*/
+
+    Dictionary<string, Dictionary<string, int>> Groups_Grades;
+    int changeCounter = 0;
 
 	// Use this for initialization
 	void Start () {
-        Populate();
+        SetGrades( "ST-QMC-101-T-001","Quimica I", 30);
+        SetGrades("ST-QMC-101-T-0012","Quimica I", 40);
+        SetGrades("ST-QMC-101-T-003", "Quimica I", 350);
+        SetGrades("ST-QMC-101-T-004", "Quimica I", 430);
+        Debug.Log(GetGrades("ST-QMC-101-T-001", "Quimica I"));
     }
 	
 	// Update is called once per frame
-	void Update () {
-        
+	void Update () {        
 	}
+    void Init()
+    {
+        if (Groups_Grades != null)
+        {
+            return;
+        }
+        Groups_Grades = new Dictionary<string, Dictionary<string, int>>();
+    }
 
-    void Populate()
+    public int GetGrades(string group_code, string group)
+    {
+        Init();
+        if(Groups_Grades.ContainsKey(group_code) == false)
+        {
+            return 0;
+        }
+        if (Groups_Grades[group_code].ContainsKey(group) == false)
+        {
+            return 0;
+        }
+
+        return Groups_Grades[group_code][group];
+    }
+
+    public void SetGrades(string group_code, string group, int grade)
+    {
+        Init();
+
+        changeCounter++;
+
+        if(Groups_Grades.ContainsKey(group_code) == false)
+        {
+            Groups_Grades[group_code] = new Dictionary<string, int>();
+        }
+        Groups_Grades[group_code][group] = grade;
+    }
+
+    public void ChangeGrades(string group_code, string group, int new_grade)
+    {
+        Init();
+        int currGrade = GetGrades(group_code, group);
+        SetGrades(group_code, group, currGrade + new_grade);
+    }
+
+    public string[] GetGroups_codes()
+    {
+        Init();
+        return Groups_Grades.Keys.ToArray();
+    }
+
+    public string[] GetGroups_codes(string SortByGrade)
+    {
+        Init();
+
+        return Groups_Grades.Keys.OrderByDescending(n => GetGrades(n, SortByGrade)).ToArray();
+
+    }
+
+    public int GetChangeCounter()
+    {
+        return changeCounter;
+    }
+
+
+
+
+    /*void Populate()
     {
         GameObject gameObject;
         for (int i = 0; i < Cant_grupos; i++)
@@ -28,6 +100,6 @@ public class Datagrid_Overview_Professor : MonoBehaviour {
             gameObject = (GameObject)Instantiate(prefab_groupcode, transform);
             gameObject = (GameObject)Instantiate(prefab_avggrades, transform);
         }
-    }
+    }*/
 
 }
---
+++ LabVirtual/Assets/Scritps/GroupList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroupList : MonoBehaviour {
+
+    public GameObject GroupEntryPrefab;
+
+    Datagrid_Overview_Professor datagrid;
+
+    int lastChangeCounter;
+
+	// Use this for initialization
+	void Start () {
+
+        datagrid = GameObject.FindObjectOfType<Datagrid_Overview_Professor>();
+
+        lastChangeCounter = datagrid.GetChangeCounter();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (datagrid.GetChangeCounter() == lastChangeCounter)
+        {
+            return;
+        }
+        else
+        {
+            lastChangeCounter = datagrid.GetChangeCounter();
+
+            while (this.transform.childCount > 0)
+            {
+                Transform firstchild = this.transform.GetChild(0);
+                firstchild.SetParent(null);
+                Destroy(firstchild.gameObject);
+            }
+
+            string[] groups = datagrid.GetGroups_codes("grade");
+
+            foreach (var group in groups)
+            {
+                GameObject gameObject = (GameObject)Instantiate(GroupEntryPrefab, transform);
+                gameObject.transform.SetParent(this.transform);
+                gameObject.transform.Find("Text_GroupCode").GetComponent<Text>().text = group;
+                gameObject.transform.Find("Text_AVGGrade").GetComponent<Text>().text = datagrid.GetGrades(group, "grade").ToString();
+
+            }
+        }
+    }
+}
