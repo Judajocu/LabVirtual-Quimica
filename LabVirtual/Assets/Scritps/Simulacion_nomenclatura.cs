86663d1e411b219380eb2dc8267d955c01d3b3a7
Melisa <melissalantigua@hotmail.com>
2018-09-23 14:03:41 -0400

    cambios en nomenclatura

---
+++ LabVirtual/Assets/Scritps/Simulacion_nomenclatura.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class Simulacion_nomenclatura : MonoBehaviour {
+
+    public Button ButtonMenu;
+
+    public GameObject menu;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValidateMenu()
+    {
+        SceneManager.LoadScene("Options_Student");
+    }
+}
