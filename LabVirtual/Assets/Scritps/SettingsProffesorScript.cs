d079117d4cb8092f6c416cff996a2fd74728cb92
Juan Joa <juandanieljoa@gmail.com>
2018-10-05 19:35:35 -0400

    pantalla de ajustes y seleccion de teoría hechas

---
+++ LabVirtual/Assets/Scritps/SettingsProffesorScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingsProffesorScript : MonoBehaviour {
+
+    #region Variables
+    public Button buttonMenu;
+    public Button buttonSubmit;
+
+    public InputField inputTiempo;
+    public InputField inputErrores;
+    public InputField inputNota;
+
+    public GameObject menu;
+    public GameObject submit;
+    public GameObject tiempo;
+    public GameObject errores;
+    public GameObject nota;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValidateMenu()
+    {
+        SceneManager.LoadScene("Options_Student");
+    }
+
+}
--- LabVirtual/Assets/Scritps/Theory_Selection.cs
+++ LabVirtual/Assets/Scritps/Theory_Selection.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Simulation_Options_Scripts : MonoBehaviour {
+public class Theory_Selection : MonoBehaviour
+{
 
     #region Variables
     public Button buttonMenu;
@@ -13,7 +14,8 @@ public class Simulation_Options_Scripts : MonoBehaviour {
     public Button buttonConversion;
     public Button buttonNomenclature;
     public Button buttonEstequimetria;
-    
+    public Button buttonIonizacion;
+
 
     public GameObject menu;
     public GameObject balance;
@@ -21,26 +23,19 @@ public class Simulation_Options_Scripts : MonoBehaviour {
     public GameObject conversion;
     public GameObject nomenclature;
     public GameObject estequimetria;
-
-    //Numeros
-    //1 es balanceo
-    //2 es tabla
-    //3 es conversion
-    //4 es nomenclatura
-    //5 es estequiometria
-    static public int selected;
+    public GameObject ionizacion;
     #endregion
-    
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-    	selected = 0;
+
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        
+
     }
 
     public void ValidateMenu()
@@ -49,34 +44,29 @@ public class Simulation_Options_Scripts : MonoBehaviour {
     }
     public void ValidateBalance()
     {
-    	selected=1;
-    	Debug.Log(selected);
         SceneManager.LoadScene("Simulation_Selection_Options");
     }
     public void ValidateTable()
     {
-    	selected=2;
-    	Debug.Log(selected);
-       	SceneManager.LoadScene("Simulation_Selection_Options");
+        SceneManager.LoadScene("Simulation_Selection_Options");
     }
     public void ValidateConversion()
     {
-    	selected=3;
-    	Debug.Log(selected);
-       	SceneManager.LoadScene("Simulation_Selection_Options");
+        SceneManager.LoadScene("Simulation_Selection_Options");
     }
     public void ValidateNomenclature()
     {
-    	selected=4;
-    	Debug.Log(selected);
         SceneManager.LoadScene("Simulation_Selection_Options");
     }
     public void ValidateEstequimetria()
     {
-    	selected=5;
-    	Debug.Log(selected);
-       	SceneManager.LoadScene("Simulation_Selection_Options");
+        SceneManager.LoadScene("Simulation_Selection_Options");
     }
-    
-   
+
+    public void ValidateIonizacion()
+    {
+        SceneManager.LoadScene("Simulation_Selection_Options");
+    }
+
+
 }
