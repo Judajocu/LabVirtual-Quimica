90794279bf1f1b9196746e611b97d560e00404af
Juan Joa <juandanieljoa@gmail.com>
2018-10-06 12:58:34 -0400

    Theory screens done

---
+++ LabVirtual/Assets/Scritps/TheoryBalanceScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class TheoryBalanceScript : MonoBehaviour {
+
+    public Button buttonMenu;
+    public Button buttonPVA;
+    public Button buttonSimulacion;
+
+    public GameObject menu;
+    public GameObject pva;
+    public GameObject simulacion;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValidateMenu()
+    {
+        SceneManager.LoadScene("Options_Student");
+    }
+}
