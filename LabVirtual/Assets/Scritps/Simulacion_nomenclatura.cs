9361036a90fea58945c9fd5e6e138acc6092aac9
Melisa <melissalantigua@hotmail.com>
2018-10-09 18:29:41 -0400

    cambios script nomenclatura

--- LabVirtual/Assets/Scritps/Simulacion_nomenclatura.cs
+++ LabVirtual/Assets/Scritps/Simulacion_nomenclatura.cs
@@ -12,14 +12,30 @@ public class Simulacion_nomenclatura : MonoBehaviour {
     public GameObject menu;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        timeup = settings.Gettime();
+        CheckType();
+        letra_a = GameObject.Find("letra_a").GetComponent<TextMesh>();
+        letra_b = GameObject.Find("letra_b").GetComponent<TextMesh>();
+        letra_c = GameObject.Find("letra_c").GetComponent<TextMesh>();
+        letra_d = GameObject.Find("letra_d").GetComponent<TextMesh>();
+        letra_e = GameObject.Find("letra_e").GetComponent<TextMesh>();
+        letra_f = GameObject.Find("letra_f").GetComponent<TextMesh>();
+        CheckTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTime();
+    }
+
+    public void CheckTime()
+    {
+        time_left += Time.deltaTime;
+        TimeOver();
+    }
 
     public void ValidateMenu()
     {
