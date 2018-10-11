0a29273fb8accd84421bef44a3a5f125f55cbaa5
Juan Joa <juandanieljoa@gmail.com>
2018-10-10 20:13:33 -0400

    Pantalla completa de estequiometría, trabajando funcionalidad

--- LabVirtual/Assets/Scritps/Estequiometria_Script.cs
+++ LabVirtual/Assets/Scritps/Estequiometria_Script.cs
@@ -1,16 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class Estequiometria_Script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    #region Variables
+    public Button ButtonMenu;
+    public Button ButtonSkip;
+    public Button Buttonsubmit;
+
+    public InputField Answer;
+
+    public GameObject menu;
+    public GameObject skip;
+    public GameObject submit;
+
+    public TextMesh cantresult;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        cantresult = GameObject.Find("Cant_result").GetComponent<TextMesh>();
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void ValidateSubmit()
+    {
+
+    }
+
+    public void ValidateSkip()
+    {
+
+    }
+
+    public void ValidateMenu()
+    {
+        SceneManager.LoadScene("Options_Student");
+    }
+
+    public void CheckResult(string result)
+    {
+        result = Answer.text;
+        Debug.Log(result);
+    }
 }
