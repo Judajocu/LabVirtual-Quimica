265dda4f5354bc4f044251dea460e7a746e19971
Juan Joa <juandanieljoa@gmail.com>
2018-11-07 22:29:29 -0400

    conversion revisada

--- LabVirtual/Assets/Scritps/OnElectronsCollide_script.cs
+++ LabVirtual/Assets/Scritps/OnElectronsCollide_script.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OnElectronsCollide_script : MonoBehaviour {
 
@@ -10,7 +11,8 @@ public class OnElectronsCollide_script : MonoBehaviour {
     public GameObject mover;
     public GameObject scale;
     public GameObject baseScale;
-    GameObject game2;
+
+    Scene activeScene;
 
     // Use this for initialization
     void Start()
@@ -37,7 +39,25 @@ public class OnElectronsCollide_script : MonoBehaviour {
             
             scale.transform.rotation = Quaternion.Euler(0,0,0);
             conversion.text = collision.transform.gameObject.GetComponentInChildren<Text>().text;
-            G2Kg(collision.gameObject);
+            switch(GetSceneName())
+            {
+                case "Conversion Nivel 1":
+                    G2Kg(collision.gameObject);
+                    break;
+                case "Conversion Nivel 2":
+                    F2C(collision.gameObject);
+                    break;
+                case "Conversion Nivel 3":
+                    Lb2g(collision.gameObject);
+                    break;
+                case "Conversion Nivel 4":
+                    Mph2Kmh(collision.gameObject);
+                    break;
+                case "Conversion Nivel 5":
+                    J2Kcal(collision.gameObject);
+                    break;
+            }
+            
         }
         answer.gameObject.SetActive(true);
     }
@@ -48,41 +68,105 @@ public class OnElectronsCollide_script : MonoBehaviour {
         if(game.gameObject.name == "Formula2" || game.gameObject.name == "Formula3" || game.gameObject.name == "Formula5")
         {
             GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
-            game1.transform.SetParent(GameObject.FindWithTag("Net").transform, false);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
         }
         if(game.gameObject.name == "Formula4" || game.gameObject.name == "Formula6")
         {
             GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
-            game1.transform.SetParent(GameObject.FindWithTag("Net").transform, false);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
         }
         if(game.gameObject.name == "Formula1")
         {
             GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
             GameObject game3 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
-            game1.transform.SetParent(GameObject.FindWithTag("Net").transform, false);
-            game3.transform.SetParent(GameObject.FindWithTag("Net").transform, false);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+            game3.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
         }
             
     }
 
-    void F2C()
+    void F2C(GameObject game)
     {
-
+        if (game.gameObject.name == "Formula2" || game.gameObject.name == "Formula4" || game.gameObject.name == "Formula6")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);            
+        }
+        if (game.gameObject.name == "Formula1" || game.gameObject.name == "Formula5")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula3")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            GameObject game3 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+            game3.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
     }
 
-    void Lb2g()
+    void Lb2g(GameObject game)
     {
-
+        if (game.gameObject.name == "Formula2" || game.gameObject.name == "Formula3" || game.gameObject.name == "Formula5")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula1" || game.gameObject.name == "Formula4")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);            
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula6")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            GameObject game3 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+            game3.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
     }
 
-    void Mph2Kmh()
+    void Mph2Kmh(GameObject game)
     {
-
+        if (game.gameObject.name == "Formula1" || game.gameObject.name == "Formula4" || game.gameObject.name == "Formula6")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula5" || game.gameObject.name == "Formula3")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula2")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            GameObject game3 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+            game3.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
     }
 
-    void J2Kcal()
+    void J2Kcal(GameObject game)
     {
-
+        if (game.gameObject.name == "Formula1" || game.gameObject.name == "Formula2" || game.gameObject.name == "Formula6")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula4" || game.gameObject.name == "Formula3")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
+        if (game.gameObject.name == "Formula5")
+        {
+            GameObject game1 = (GameObject)Instantiate(mover, new Vector3(-4.03f, 2.474f, 0f), Quaternion.identity);
+            GameObject game3 = (GameObject)Instantiate(mover, new Vector3(-2.281f, 2.474f, 0f), Quaternion.identity);
+            game1.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+            game3.transform.SetParent(GameObject.FindWithTag("Net2").transform, false);
+        }
     }
 
     void Clean()
@@ -90,5 +174,11 @@ public class OnElectronsCollide_script : MonoBehaviour {
         GameObject[] prefavs = GameObject.FindGameObjectsWithTag("Respawn");
         foreach (GameObject enemy in prefavs)
             GameObject.Destroy(enemy);
+    }
+
+    public string GetSceneName()
+    {
+        activeScene = SceneManager.GetActiveScene();
+        return activeScene.name;
     }
 }
