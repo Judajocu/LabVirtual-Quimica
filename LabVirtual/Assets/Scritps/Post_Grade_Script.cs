2cdde885002a0d9d62b32fca247fcc5c85818eab
judajocu <juandanieljoa@gmail.com>
2018-11-19 20:47:42 -0400

    POST REQUEST

---
+++ LabVirtual/Assets/Scritps/Post_Grade_Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+using System.IO;
+using System.Text;
+
+public class Post_Grade_Script : MonoBehaviour
+{
+
+    static string url = "https://jsonblob.com/api/jsonBlob";
+
+    void PostData()
+    {
+        string path = Application.persistentDataPath + "/Nota Simulation.json";
+        string jsonString = File.ReadAllText(path);
+        Hashtable headers = new Hashtable();
+        headers.Add("Content-Type", "application/json");
+        byte[] body = Encoding.UTF8.GetBytes(jsonString);
+        WWW www = new WWW(url, body, headers);
+        StartCoroutine("PostdataEnumerator", www);
+    }
+    IEnumerator PostdataEnumerator(WWW www)
+    {
+        yield return www;
+        if (www.error != null)
+        {
+            Debug.Log("Data Submitted");
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+}
