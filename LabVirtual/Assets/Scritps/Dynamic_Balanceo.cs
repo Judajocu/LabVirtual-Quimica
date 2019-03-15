64b7d22d22922473576d72fd568b39c06315dcf0
Juan Joa <juandanieljoa@gmail.com>
2019-03-15 11:30:34 -0400

    escenas de dinamismo de balanceo y estequiometria

--- LabVirtual/Assets/Scritps/Dynamic_Balanceo.cs
+++ LabVirtual/Assets/Scritps/Dynamic_Balanceo.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Dynamic_Balanceo : MonoBehaviour {
 
+    #region Varialbes
     public Button buttonSave;
+    public InputField Sol;
+    public InputField Prob;
+    public Dropdown Nivels;
+    public TMP_InputField Pro;
 
     public GameObject ButtonAceptar;
 
-	// Use this for initialization
-	void Start () {
+    string sol;
+    string prob;
+    string nivels;
+    string context;
+    #endregion
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -23,6 +36,26 @@ public class Dynamic_Balanceo : MonoBehaviour {
     public void ValidateChange()
     {
         SceneManager.LoadScene("Dynamic_Simulations");
+    }
+    //Esto consigue el texto de la solucion despues que se cambia
+    public void GetSol()
+    {
+        sol = Sol.text;
+    }
+    //Esto consigue el texto del problema despues que se cambia
+    public void GetProb()
+    {
+        prob = Prob.text;
+    }
+    //Esto consigue el texto del dropdown
+    public void GetNivel()
+    {
+        nivels = Nivels.options[Nivels.value].text;
+    }
+    //Esto consigue lo del contexto
+    public void GetContext()
+    {
+        context = Pro.text;
     }
 
 }
