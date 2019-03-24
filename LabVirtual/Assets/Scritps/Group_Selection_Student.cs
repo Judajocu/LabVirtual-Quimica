de2cca8bb080353b16f41ddd97b653ea359aac97
Juan Joa <juandanieljoa@gmail.com>
2019-03-23 20:19:16 -0400

    pantalla y limpieza

---
+++ LabVirtual/Assets/Scritps/Group_Selection_Student.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Group_Selection_Student : MonoBehaviour {
+
+    #region Variables
+    public Dropdown dropdown;
+    public Button Exit;
+    public Button Submit;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ValidateSubmit()
+    {
+        SceneManager.LoadScene("Overview-Student");
+    }
+
+    public void ValidateExit()
+    {
+        SceneManager.LoadScene("Login-Student");
+    }
+}
